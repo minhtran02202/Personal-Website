efe80d7a0874f24aadc328db92beb6f290d24b78
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2023-05-20 15:28:06 -0700

    sizing elements in contact

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const date = new Date();
+  console.log(date);
   const submitButton = document.querySelector("#submit");
+  const name = document.querySelector("#subject");
   submitButton.addEventListener("click", () => {
-    alert("Submit Successfully!");
+    alert(`Submit Successfully! Thanks for visiting, ${name.value}`);
   });
 });
