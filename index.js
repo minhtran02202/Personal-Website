365dff5ce21f3f859c57036cc10c5218ec5fedee
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2023-05-20 15:08:15 -0700

    add links to project images and alert window when submit contact successfully

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const submitButton = document.querySelector("#submit");
+  submitButton.addEventListener("click", () => {
+    alert("Submit Successfully!");
+  });
+});
